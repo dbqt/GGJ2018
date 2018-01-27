3a5523dc4ffb49e7b3341e80ff136dfb8d68aae5
Steven Lam <lam.steven7@gmail.com>
2018-01-27 16:43:12 -0500

    Created and started player controller script

---
+++ Assets/GameInGame/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0.0f, moveZ);
+
+        rb.AddForce(movement * speed);
+    }
+}
