aafabe1c891433b9d7646dafb1936296fc8a43e0
BrandonCHW <brad_1800@hotmail.com>
2018-01-27 11:19:48 -0500

    Skeleton dropGM

--- Assets/ColorPhysic/Scripts/dropGM.cs
+++ Assets/ColorPhysic/Scripts/dropGM.cs
@@ -2,15 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class dropGM : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static dropGM instance { get; set; }
+
+    enum StateType
+    {
+        MENU,
+        TUTO1,
+        TUTO2,
+        TUTO3,
+        LEVEL1,
+        LEVEL2
+    };
+
+    [SerializeField]
+    private StateType state;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        ResetAll();
+        state = StateType.MENU;
+    }
+
+    void GoToMenu()
+    {
+
+    }
+
+    void GoToTuto1()
+    {
+
+    }
+
+    void GoToTuto2()
+    {
+
+    }
+
+    void GoToTuto3()
+    {
+
+    }
+    
+    void GoToLevel1()
+    {
+
+    }
+
+    void GoToLevel2()
+    {
+
+    }
+
+    void ResetAll()
+    {
+    }
 }
