c3903f60d60084ee281b1765f49517cd70d10034
InternalEcho <24857190+InternalEcho@users.noreply.github.com>
2018-01-27 15:16:03 -0500

    Finished a basic timer.

---
+++ Assets/GameInGame/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft = 30.0f;
+
+    void Update()
+    {
+        Countdown();
+    }
+
+    private void Countdown()
+    {
+        timeLeft -= Time.deltaTime;
+        Debug.Log(timeLeft);
+        if (timeLeft < 0)
+        {
+            // do something
+        }
+    }
+}
