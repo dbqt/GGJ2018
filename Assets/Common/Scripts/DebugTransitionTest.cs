3086ca1b5ce5b38c43d1ddd9ac30320be3a2fb4e
David Tran <dt_dbqt@hotmail.com>
2018-01-27 15:59:31 -0500

    transition done

--- Assets/Common/Scripts/DebugTransitionTest.cs
+++ Assets/Common/Scripts/DebugTransitionTest.cs
@@ -3,22 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DebugTransitionTest : MonoBehaviour {
-
-    [SerializeField]
-    float Delay = 2f;
-
     [SerializeField]
     string SceneToLoad;
 
-	// Use this for initialization
 	void Start () {
-		GameTransition.Instance.PrepareLoadLevel(SceneToLoad);
-        ZoomToCenter();
-
-        Invoke("Load", Delay);
+		GameTransition.Instance.LoadLevel(SceneToLoad);
 	}
-
-    void Load() {
-        GameTransition.Instance.ActivateLoadLevel();
-    }
 }
