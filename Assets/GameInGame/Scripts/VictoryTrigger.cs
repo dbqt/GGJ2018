ef3ea7083cd6358be9f621c630978985592d53e4
David Tran <dt_dbqt@hotmail.com>
2018-01-28 14:06:30 -0500

    fix using

--- Assets/GameInGame/Scripts/VictoryTrigger.cs
+++ Assets/GameInGame/Scripts/VictoryTrigger.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManager;
 public class VictoryTrigger : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
