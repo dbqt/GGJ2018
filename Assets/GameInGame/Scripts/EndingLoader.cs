53b88a0e5cee7ff557f844d3d1fbabbc7237c34a
David Tran <dt_dbqt@hotmail.com>
2018-01-28 13:37:33 -0500

    updated script for ending

--- Assets/GameInGame/Scripts/EndingLoader.cs
+++ Assets/GameInGame/Scripts/EndingLoader.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EndingLoader : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void OnEnable () {
 		GameManager.Instance.GoToMenu();
         GameTransition.Instance.ActivateLoadLevel();
 	}
