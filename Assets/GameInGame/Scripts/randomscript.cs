c6f2e1b989be273b20a18c5d9f8f6cfc2a5d1b03
okaymandontmakemedothis <babysheebo@gmail.com>
2018-01-31 11:09:21 -0500

    Trying out the [user] section in the local repository .git/config file

--- Assets/GameInGame/Scripts/randomscript.cs
+++ Assets/GameInGame/Scripts/randomscript.cs
@@ -7,3 +7,4 @@ cjsa;
 plswork
 should work this time *crosses fingers*
 okay maybe link to github acc this time?!? onegaishimasu
+removed ~/.gitignore, added [user] section to the local repository config file in .git
